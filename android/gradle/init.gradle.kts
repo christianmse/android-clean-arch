0918dd64fdcf697b5cb4e9f8dd884719fbeac885
Christian Sandoval <christian_sandoval@epam.com>
2023-10-02 11:38:51 +0200

    Static analysis pre-push

--- android/gradle/init.gradle.kts
+++ android/gradle/init.gradle.kts
@@ -1,15 +1,18 @@
 val ktlintVersion = "0.48.1"
 
 initscript {
-    plugins {
-        id("com.diffplug.gradle.spotless") version "6.22.0"
+    val spotlessVersion = "6.22.0"
+    repositories {
+        mavenCentral()
+    }
+
+    dependencies {
+        classpath("com.diffplug.spotless:spotless-plugin-gradle:$spotlessVersion")
     }
 }
 
 // Apply plugin to all project modules
 rootProject {
-    ratchetFrom 'origin/master'
-
     subprojects {
         apply<com.diffplug.gradle.spotless.SpotlessPlugin>()
         extensions.configure<com.diffplug.gradle.spotless.SpotlessExtension> {
