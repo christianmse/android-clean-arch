87028fe94d26c71f5ccbf93860963f672dcac1d1
Christian Sandoval <christian_sandoval@epam.com>
2023-10-02 11:09:24 +0200

    Git pre-commit hook added

--- android/gradle/init.gradle.kts
+++ android/gradle/init.gradle.kts
@@ -1,18 +1,15 @@
 val ktlintVersion = "0.48.1"
 
 initscript {
-    val spotlessVersion = "6.13.0"
-
-    repositories {
-        mavenCentral()
-    }
-
-    dependencies {
-        classpath("com.diffplug.spotless:spotless-plugin-gradle:$spotlessVersion")
+    plugins {
+        id("com.diffplug.gradle.spotless") version "6.22.0"
     }
 }
 
+// Apply plugin to all project modules
 rootProject {
+    ratchetFrom 'origin/master'
+
     subprojects {
         apply<com.diffplug.gradle.spotless.SpotlessPlugin>()
         extensions.configure<com.diffplug.gradle.spotless.SpotlessExtension> {
@@ -20,16 +17,12 @@ rootProject {
                 target("**/*.kt")
                 targetExclude("**/build/**/*.kt")
                 ktlint(ktlintVersion).userData(mapOf("android" to "true"))
-            }
-            format("kts") {
-                target("**/*.kts")
-                targetExclude("**/build/**/*.kts")
-                // Look for the first line that doesn't have a block comment (assumed to be the license)
+                trimTrailingWhitespace()
+                indentWithSpaces()
             }
             format("xml") {
                 target("**/*.xml")
                 targetExclude("**/build/**/*.xml")
-                // Look for the first XML tag that isn't a comment (<!--) or the xml declaration (<?xml)
             }
         }
     }
