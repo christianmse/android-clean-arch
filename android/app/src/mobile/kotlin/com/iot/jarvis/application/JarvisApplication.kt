6d825c585ea2c394f2d225fe47ea5955324c5419
Christian Sandoval <christian_sandoval@epam.com>
2023-10-02 11:40:14 +0200

    Draft

--- android/app/src/mobile/kotlin/com/iot/jarvis/application/JarvisApplication.kt
+++ android/app/src/mobile/kotlin/com/iot/jarvis/application/JarvisApplication.kt
@@ -2,6 +2,6 @@ package com.iot.jarvis.application
 
 class JarvisApplication: BaseJarvisApplication() {
     override fun onCreate() {
-        super.onCreate()
+                                            super.onCreate()
     }
 }
